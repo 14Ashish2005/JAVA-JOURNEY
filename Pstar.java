c0fe770d1b25451578fa9e449c611c3785d4ba48
Ashish kumar <2005ashish2005@gmail.com>
2026-08-17 23:55:30 +0530

    Duplicate star pattern output in Pstar.java
    Added an additional print statement to display the pattern again.

--- Pstar.java
+++ Pstar.java
@@ -2,6 +2,7 @@ public class Pstar {
   public  static void main(String [] args) {
       System.out.println("*\n**\n***\n****\n*****");
       System.out.println("*****\n****\n***\n**\n*");
+    System.out.println("*****\n****\n***\n**\n*");
 
     }
 }
